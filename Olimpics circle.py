d32e23c3dba40480f9cf9777dfde96ddcc7c71fa
luissanchez1908 <luissanchez1908@gmail.com>
2018-10-24 12:55:52 -0400

    Add files via upload

---
+++ Olimpics circle.py
@@ -0,0 +1,36 @@
+import turtle
+
+
+turtle.width(7)
+turtle.shape("turtle")
+#first circle
+turtle.color("green")
+turtle.circle(50)
+
+#second circle 
+turtle.penup()
+turtle.setposition(-120, 0)
+turtle.pendown()
+turtle.color("yellow")
+turtle.circle(50)
+
+#third circle 
+turtle.penup()
+turtle.setposition(60,60)
+turtle.pendown()
+turtle.color("red")
+turtle.circle(50)
+ 
+#fourth circle
+turtle.penup()
+turtle.setposition(-60, 60)
+turtle.pendown()
+turtle.color("black")
+turtle.circle(50)
+
+#fifth circle 
+turtle.penup()
+turtle.setposition(-180, 60)
+turtle.pendown()
+turtle.color("blue")
+turtle.circle(50)
